826a7e6e4fa114a81de3002366bef1933e032aa2
str8ndrew <str8ndrew@users.noreply.github.com>
2024-01-11 17:41:04 +0100

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,9 @@
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding);
-const borderWidth = parseFloat(border);
-const totalWidth = content * padding * border;
-
-
-
 function getElementWidth(content, padding, border) {
-    return(`${content} + ${padding} + ${border}`)
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    return totalWidth;
 }
 
 
