3e9bf7d39def498616ba6bf3325971799df57c09
str8ndrew <str8ndrew@users.noreply.github.com>
2024-01-11 20:34:43 +0100

    update task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,8 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return (`You ordered ${quantity} droids worth ${pricePerDroid} credits!`)
+    const totalPrice = quantity * pricePerDroid;
+    return (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
 }
+
 
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
